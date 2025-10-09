984b58c6f753421c14a6fe7a8827a13ae7777838
Anna Clara Dos Santos Vieira <20231TIIMI0375@cefetes.br>
2025-10-09 12:53:31 -0300

    Initial commit

---
+++ app/src/main/java/data/Product.kt
@@ -0,0 +1,10 @@
+package data
+
+data class Product(
+    val id: String,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val imageUrl: String,
+    val rating: Double
+)
